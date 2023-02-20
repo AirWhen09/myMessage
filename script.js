fe22803d1791f9984d1c67387464ef41e3cda081
Erwin Ibarra <98625625+AirWhen09@users.noreply.github.com>
2023-02-20 09:07:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-let phrases = ["Hello", "Good Day", "\"You are never too old to set another goal or to dream a new dream.\" - C.S. Lewis", "\"The only way to do great work is to love what you do.\" - Steve Jobs ", "\"If you want to lift yourself up, lift up someone else.\" - Booker T. Washington", "Keep Safe!!", "-Erwin", "😍"]
+let phrases = ["Hello", "Good Day", "You are never too old", "to set another goal", " or to dream a new dream.  - C.S. Lewis", "Keep Safe!!", "-Erwin", "🙏👻"]
 let demo = document.querySelector(".demo")
-let animation = gsap.timeline({repeat:5, repeatDelay:7, delay:5})
+let animation = gsap.timeline({repeat:5, repeatDelay:7,})
 
 function createLayers(){
   phrases.forEach(value => {
@@ -14,7 +14,7 @@ function createLayers(){
 function animateText() {
   let layers = document.querySelectorAll(".demo div")
   layers.forEach(function(element, index){
-    animation.fromTo(element, {opacity:0, scale:0}, {scale:1, opacity:1, repeat:1, yoyo:true, yoyoEase:true, repeatDelay:0.3, delay: 5})
+    animation.fromTo(element, {opacity:0, scale:0}, {scale:1, opacity:1, repeat:1, yoyo:true, yoyoEase:true, repeatDelay:0.3})
   })
   gsap.set(".demo", {visibility:"visible"}) 
 }
